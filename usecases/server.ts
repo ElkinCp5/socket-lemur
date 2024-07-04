e6b10afdc2a25a7c2cbd69fc8ec7ed4ea6b63a94
elkincp5 <elkincp5@gmail.com>
2024-07-04 14:03:32 -0500

    Use cases

--- usecases/server.ts
+++ usecases/server.ts
@@ -2,13 +2,13 @@ import { SocketServer } from '../';
 
 
 type Session = { id: string };
-const socket = new SocketServer<Session>('api-key', 'jwt-secret');
-
 const PORT = process.env.PORT || 3030
-// Start the server on port
-socket.listen(PORT, () => {
-    console.log(`Server Run http://localhost:${PORT}`);
+const server = new SocketServer<Session>({
+    apikey: 'api-key',
+    secret: 'jwt-secret',
+    roomsEnabled: true
 });
+
 
 async function get() {
     return data;
@@ -21,12 +21,81 @@ async function add(product: { name: string }) {
 
 const data = [{ id: 1, name: 'Pizza' }, { id: 2, name: 'Pasta' }];
 
-socket.channel<any>('get/products', async (_, res) => {
+// Channel Defained
+server.channel<any>('get/products', async (_, res) => {
     const products = await get();
     res(products)
 });
 
-socket.channel<{ name: string }>('post/products', async (req, res) => {
+server.channel<{ name: string }>('post/products', async (req, res) => {
     const products = await add(req.body);
     res(products);
-}, true);
+});
+
+// Server run on port
+server.listen(PORT, () => {
+    console.log(`Server Run http://localhost:${PORT}`);
+});
+
+// on Connection
+server.connection({
+    on: () => {
+        console.log("onConnection")
+    },
+    off: () => {
+        console.log("onDisconnect")
+    }
+});
+
+// import http from 'http';
+// import { Server } from 'socket.io';
+
+// const server = http.createServer((_, res) => {
+//     res.writeHead(404);
+//     res.end();
+// });
+
+// const io = new Server(server, {
+//     cors: {
+//         origin: "*",
+//         methods: ["GET", "POST"]
+//     }
+// });
+
+// const PORT = process.env.PORT || 3030;
+
+// // Datos de ejemplo
+// const data = [{ id: 1, name: 'Pizza' }, { id: 2, name: 'Pasta' }];
+
+// async function get() {
+//     return data;
+// }
+
+// async function add(product: any) {
+//     data.push({ id: data.length + 1, name: product.name });
+//     return data;
+// }
+
+// io.on('connection', (socket) => {
+//     console.log('connection');
+
+//     // Manejar solicitud de añadir producto
+//     socket.on('post/products', async (product) => {
+//         const products = await add(product);
+//         socket.emit('get/products::success', products);
+//     });
+
+//     // Manejar solicitud de obtener productos
+//     socket.on('get/products', async () => {
+//         const products = await get();
+//         socket.emit('get/products::success', products);
+//     });
+
+//     socket.on('disconnect', () => {
+//         console.log('Un cliente se ha desconectado');
+//     });
+// });
+
+// server.listen(PORT, () => {
+//     console.log(`Servidor escuchando en http://localhost:${PORT}`);
+// });
