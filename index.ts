ed862a812a7cfe8210aeee98dcb4947871f5ac09
Elkin Portocarrero <elkincp5@gmail.com>
2025-01-26 12:11:53 -0500

    Exports ExpiringMap, Logger, WebPushLemur

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
 export { SocketClient } from "./src/socketClient"
 export { SocketServer } from "./src/socketServer"
 export { TokenManager } from "./src/tokenManager"
+export { ExpiringMap } from "./src/lib/expiring-map"
+
+export { Logger } from './src/lib/logger';
+export { WebPushLemur } from './src/lib/web-push-lemur';
