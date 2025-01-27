71c7f90a5f20573a2392e9e4dd79ac8ef55c5130
Elkin Portocarrero <elkincp5@gmail.com>
2025-01-27 17:48:23 -0500

    We extract the LogLevel enum from the logger.d.ts file to avoid errors in the tests

--- src/dts/logger.d.ts
+++ src/dts/logger.d.ts
@@ -1,9 +1,3 @@
-export declare enum LogLevel {
-    INFO = "INFO",
-    ERROR = "ERROR",
-    WARNING = "WARNING",
-}
-
 export declare interface LoggerSystem {
     log(level: LogLevel, message: string, meta?: any): Promise<void>;
     error(message: string, error?: any): Promise<void>;
